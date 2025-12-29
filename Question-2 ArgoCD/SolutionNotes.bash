932b641644510f7af18a6ba57f81eaf049fc6934
Hiro <webmaster@supaahiro.org>
2025-12-29 14:03:41 +0100

    docs(argo): update SolutionNotes with additional commands

--- Question-2 ArgoCD/SolutionNotes.bash
+++ Question-2 ArgoCD/SolutionNotes.bash
@@ -6,3 +6,23 @@ helm repo add argocd https://argoproj.github.io/argo-helm
 helm repo update
 helm template argocd argo/argo-cd --version 7.7.3 --set crds.install=false --namespace argocd > /root/argo-helm.yaml
 cat /root/argo-helm.yaml   # confirm output
+
+# Addictional Notes
+
+# How can I list the charts of a repo?
+helm search repo argocd
+
+# How can I list the versions of a chart?
+helm search repo argocd/argo-cd --versions
+
+# How can I see the README of a chart?
+helm show readme argo/argo-cd --version 7.7.3
+
+# How can I see the default values of a chart?
+helm show values argo/argo-cd --version 7.7.3
+
+# Install ArgoCD without CRDs
+helm install argocd argocd/argo-cd --version=7.7.3 -n argocd --set crds.install=false
+
+# How can I validate the generated manifests?
+k create -f /root/argo-helm.yaml --dry-run=server
